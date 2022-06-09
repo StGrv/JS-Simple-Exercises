f1d49a2519130be613515f344c3e0acdb982b768
StGrv <65493257+StGrv@users.noreply.github.com>
2022-06-09 15:01:12 +0300

    Add files via upload

---
+++ Last-owl-item.js
@@ -0,0 +1,17 @@
+findLastActiveItem();
+$(".owl-carousel").on('translated.owl.carousel', function (event) {
+    findLastActiveItem();
+});
+
+function findLastActiveItem() {
+    var total = $('.owl-carousel.owl-stage .owl-item.active').length;
+
+    $('.owl-carousel .owl-stage .owl-item').removeClass('last-active-item');
+
+    $('.owl-carousel .owl-stage .owl-item.active').each(function (index) {
+        if (index === total - 1 && total > 1) {
+            // Last active item from the carousel
+            $(this).addClass('last-active-item');
+        }
+    });
+}
